8f6dcdefabf5968e475ea6911af7ee94a8132b48
Andres Mechali <andresmechali@gmail.com>
2023-06-30 22:09:27 +0200

    create basic todo layout and fetch dummy data

---
+++ app/todos/[todoId]/page.tsx
@@ -0,0 +1,33 @@
+import { Todo } from "@/typings";
+
+type PageProps = {
+  params: {
+    todoId: string;
+  };
+};
+
+const fetchTodo = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+
+  const todo: Todo = await res.json();
+
+  return todo;
+};
+
+export default async function TodoPage({ params: { todoId } }: PageProps) {
+  const todo = await fetchTodo(todoId);
+
+  return (
+    <div className="p-10 bg-yellow-100 border-2 m-2 shadow-lg text-black">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+      <p className="border-t border-black mt-5 text-right">
+        By user: {todo.userId}
+      </p>
+    </div>
+  );
+}
---
+++ app/todos/page.tsx
@@ -0,0 +1,9 @@
+import TodosList from "@/app/todos/TodosList";
+
+export default function Todos() {
+  return (
+    <div>
+      <TodosList />
+    </div>
+  );
+}
